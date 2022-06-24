323d8749b6f779884fac562473eac6a5352f0d77
Henry Cooper <henrycooper@outlook.com>
2022-06-24 13:53:57 +0100

    Initial Commit

---
+++ Dylan/DylanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DylanApp.swift
+//  Dylan
+//
+//  Created by Henry Cooper on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DylanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
